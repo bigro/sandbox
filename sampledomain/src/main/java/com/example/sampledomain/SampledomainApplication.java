91be35024b30cda1cbadb53d69e79b95f1e79199
bigro <kitkat.jaja@gmail.com>
2021-02-27 17:56:03 +0900

    サンプルドメインプロジェクト追記

---
+++ sampledomain/src/main/java/com/example/sampledomain/SampledomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampledomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampledomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampledomainApplication.class, args);
+	}
+
+}
